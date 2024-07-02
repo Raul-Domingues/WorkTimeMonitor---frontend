dfd470cee56475704c5fca082b4958220acf3e76
Raul Domingues <raul.domingues@db1.com.br>
2024-07-01 22:35:50 -0300

    feat: criação de componentes, rotas, importações, etc.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TeamRegistrationComponent } from './pages/team-registration/team-registration.component';
+import { TimeManagementComponent } from './pages/time-management/time-management.component';
+import { OverviewComponent } from './pages/overview/overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'cadastro-membros',
+    component: TeamRegistrationComponent
+  },
+  {
+    path: 'gerenciamento-horas',
+    component: TimeManagementComponent
+  },
+  {
+    path: 'resumo',
+    component: OverviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamRegistrationComponent } from './pages/team-registration/team-registration.component';
+import { TimeManagementComponent } from './pages/time-management/time-management.component';
 import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamRegistrationComponent,
-    HeaderComponent
+    TimeManagementComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
   ],
   providers: [
     provideClientHydration()
